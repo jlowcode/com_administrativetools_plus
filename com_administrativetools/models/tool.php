6e711927f1c5e09e2195c3533b5eb9f0224f6f4b
marcelomirandasilva <marcelo.miranda.pp@gmail.com>
2023-03-10 11:47:34 -0300

    CleanDB

---
+++ com_administrativetools/models/tool.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @version    CVS: 1.0.0
+ * @package    Com_Administrativetools
+ * @author     Hirlei Carlos Pereira de Araújo <prof.hirleicarlos@gmail.com>
+ * @copyright  2020 Hirlei Carlos Pereira de Araújo
+ * @license    GNU General Public License versão 2 ou posterior; consulte o arquivo License. txt
+ */
+// No direct access.
+defined('_JEXEC') or die;
+
+jimport('joomla.application.component.modelitem');
+jimport('joomla.event.dispatcher');
+
+use \Joomla\CMS\Factory;
+use \Joomla\Utilities\ArrayHelper;
+use \Joomla\CMS\Language\Text;
+use \Joomla\CMS\Table\Table;
+
+/**
+ * Administrativetools model.
+ *
+ * @since  1.6
+ */
+class AdministrativetoolsModelTool extends \Joomla\CMS\MVC\Model\ItemModel
+{
+
+}
---
+++ com_administrativetools/views/tools/view.html.php
@@ -0,0 +1,133 @@
+<?php
+
+/**
+ * @version    CVS: 1.0.0
+ * @package    Com_Administrativetools
+ * @author     Hirlei Carlos Pereira de Araújo <prof.hirleicarlos@gmail.com>
+ * @copyright  2020 Hirlei Carlos Pereira de Araújo
+ * @license    GNU General Public License versão 2 ou posterior; consulte o arquivo License. txt
+ */
+// No direct access
+defined('_JEXEC') or die;
+
+jimport('joomla.application.component.view');
+
+use \Joomla\CMS\Factory;
+use \Joomla\CMS\Language\Text;
+
+/**
+ * View class for a list of Administrativetools.
+ *
+ * @since  1.6
+ */
+class AdministrativetoolsViewTools extends \Joomla\CMS\MVC\View\HtmlView
+{
+	protected $items;
+
+	protected $pagination;
+
+	protected $state;
+
+	protected $params;
+
+	/**
+	 * Display the view
+	 *
+	 * @param   string  $tpl  Template name
+	 *
+	 * @return void
+	 *
+	 * @throws Exception
+     * @since    1.6
+	 */
+	public function display($tpl = null)
+	{
+		$app = Factory::getApplication();
+
+		$this->state = $this->get('State');
+		$this->params = $app->getParams('com_administrativetools');
+		
+
+		// Check for errors.
+		if (count($errors = $this->get('Errors')))
+		{
+			throw new Exception(implode("\n", $errors));
+		}
+
+		$this->_prepareDocument();
+		parent::display($tpl);
+	}
+
+	/**
+	 * Prepares the document
+	 *
+	 * @return void
+	 *
+	 * @throws Exception
+     * @since    1.6
+	 */
+	protected function _prepareDocument()
+	{
+		$app   = Factory::getApplication();
+		$menus = $app->getMenu();
+		$title = null;
+
+		// Because the application sets a default page title,
+		// we need to get it from the menu item itself
+		$menu = $menus->getActive();
+
+		if ($menu)
+		{
+			$this->params->def('page_heading', $this->params->get('page_title', $menu->title));
+		}
+		else
+		{
+			$this->params->def('page_heading', Text::_('COM_ADMINISTRATIVETOOLS_DEFAULT_PAGE_TITLE'));
+		}
+
+		$title = $this->params->get('page_title', '');
+
+		if (empty($title))
+		{
+			$title = $app->get('sitename');
+		}
+		elseif ($app->get('sitename_pagetitles', 0) == 1)
+		{
+			$title = Text::sprintf('JPAGETITLE', $app->get('sitename'), $title);
+		}
+		elseif ($app->get('sitename_pagetitles', 0) == 2)
+		{
+			$title = Text::sprintf('JPAGETITLE', $title, $app->get('sitename'));
+		}
+
+		$this->document->setTitle($title);
+
+		if ($this->params->get('menu-meta_description'))
+		{
+			$this->document->setDescription($this->params->get('menu-meta_description'));
+		}
+
+		if ($this->params->get('menu-meta_keywords'))
+		{
+			$this->document->setMetadata('keywords', $this->params->get('menu-meta_keywords'));
+		}
+
+		if ($this->params->get('robots'))
+		{
+			$this->document->setMetadata('robots', $this->params->get('robots'));
+		}
+	}
+
+	/**
+	 * Check if state is set
+	 *
+	 * @param   mixed  $state  State
+	 *
+	 * @return bool
+     * @since    1.6
+	 */
+	public function getState($state)
+	{
+		return isset($this->state->{$state}) ? $this->state->{$state} : false;
+	}
+}
